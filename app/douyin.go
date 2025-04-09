d11623bfd9db268d8c41336d02d6c54f63f2ccce
iiecho1 <hkwsvip@gmail.com>
2025-04-09 20:26:12 +0800

    优化部分代码

--- app/douyin.go
+++ app/douyin.go
@@ -5,9 +5,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"math"
 	"net/http"
 )
+
+type Douyinresponse struct {
+	WordList []Douyindata `json:"word_list"`
+}
+
+type Douyindata struct {
+	Title    string  `json:"word"`
+	HotVaule float64 `json:"hot_value"`
+}
 
 func Douyin() map[string]interface{} {
 	url := "https://www.iesdouyin.com/web/api/v2/hotsearch/billboard/word/"
@@ -18,23 +26,23 @@ func Douyin() map[string]interface{} {
 	pageBytes, err := io.ReadAll(resp.Body)
 	utils.HandleError(err, "io.ReadAll")
 
-	var resultMap map[string]interface{}
+	var resultMap Douyinresponse
 	_ = json.Unmarshal(pageBytes, &resultMap)
 
-	wordList := resultMap["word_list"]
+	wordList := resultMap.WordList
 
 	api := make(map[string]interface{})
 	api["code"] = 200
 	api["message"] = "抖音"
 
 	var obj []map[string]interface{}
-	for index, item := range wordList.([]interface{}) {
+	for index, item := range wordList {
 		result := make(map[string]interface{})
 		result["index"] = index + 1
-		result["title"] = item.(map[string]interface{})["word"]
-		hot := item.(map[string]interface{})["hot_value"].(float64) / 10000
-		result["hotValue"] = fmt.Sprint(math.Round(hot*10)/10) + "万"
-		result["url"] = "https://www.douyin.com/search/" + item.(map[string]interface{})["word"].(string)
+		result["title"] = item.Title
+		hot := item.HotVaule / 10000
+		result["hotValue"] = fmt.Sprint(hot) + "万"
+		result["url"] = "https://www.douyin.com/search/" + item.Title
 		obj = append(obj, result)
 	}
 	api["obj"] = obj
--- app/nanfangzhoumo.go
+++ app/nanfangzhoumo.go
@@ -8,6 +8,17 @@ import (
 	"strconv"
 )
 
+type Response struct {
+	NfzmData Data `json:"data"`
+}
+type Data struct {
+	HotContents []contents `json:"hot_contents"`
+}
+type contents struct {
+	Title string  `json:"subject"`
+	ID    float64 `json:"id"`
+}
+
 func Nanfangzhoumo() map[string]interface{} {
 	url := "https://www.infzm.com/hot_contents?format=json"
 	resp, err := http.Get(url)
@@ -16,21 +27,21 @@ func Nanfangzhoumo() map[string]interface{} {
 	// 2.读取页面内容
 	pageBytes, err := io.ReadAll(resp.Body)
 	utils.HandleError(err, "io.ReadAll")
-	var resultMap map[string]interface{}
+	var resultMap Response
 	_ = json.Unmarshal(pageBytes, &resultMap)
 
-	wordList := resultMap["data"].(map[string]interface{})["hot_contents"]
+	wordList := resultMap.NfzmData.HotContents
 
 	api := make(map[string]interface{})
 	api["code"] = 200
 	api["message"] = "南方周末"
 
 	var obj []map[string]interface{}
-	for index, item := range wordList.([]interface{}) {
+	for index, item := range wordList {
 		result := make(map[string]interface{})
 		result["index"] = index + 1
-		result["title"] = item.(map[string]interface{})["subject"]
-		result["url"] = "https://www.infzm.com/contents/" + strconv.FormatFloat(item.(map[string]interface{})["id"].(float64), 'f', -1, 64)
+		result["title"] = item.Title
+		result["url"] = "https://www.infzm.com/contents/" + strconv.FormatFloat(item.ID, 'f', -1, 64)
 		obj = append(obj, result)
 	}
 	api["obj"] = obj
