8446db5178552f6a4fadfc33024914717dec5c82
iiecho1 <hkwsvip@gmail.com>
2025-04-11 21:09:00 +0800

    优化部分代码

--- app/ithome.go
+++ app/ithome.go
@@ -16,18 +16,19 @@ func Ithome() map[string]interface{} {
 	pattern := `<p class="plc-title">(.*?)<\/p>.*?<a href="(.*?)"`
 	matches := utils.ExtractMatches(string(pageBytes), pattern)
 
-	api := make(map[string]interface{})
-	api["code"] = 200
-	api["message"] = "It之家"
 	var obj []map[string]interface{}
 	for index, item := range matches[:12] {
-		result := make(map[string]interface{})
-		result["index"] = index + 1
-		result["title"] = item[1]
-		result["url"] = item[2]
-		obj = append(obj, result)
+		obj = append(obj, map[string]interface{}{
+			"index": index + 1,
+			"title": item[1],
+			"url":   item[2],
+		})
 	}
-	api["obj"] = obj
-	api["icon"] = "https://www.ithome.com/favicon.ico" // 32 x 32
+	api := map[string]interface{}{
+		"code":    200,
+		"message": "IT之家",
+		"icon":    "https://www.ithome.com/favicon.ico", // 32 x 32
+		"obj":     obj,
+	}
 	return api
 }
